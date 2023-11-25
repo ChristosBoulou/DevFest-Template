f009ae905ec49e80aa3c46a2516acc1b9689bd1e
christosbouloumpasis <christosbouloumpasis@gmail.com>
2023-11-25 09:43:49 +0000

    Schedule changesz

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,12 @@ import Hero from '../components/hero.js';
 
 import SponsorLogos from '../components/sponsors.js';
 import LocationSection from '../components/location.js';
+import Schedule from "./schedule";
 
 const Home = () => (
   <div>
     <Hero />
+    <Schedule/>
     {/*<About />*/}
     {/*<SpeakerProfiles />*/}
     <LocationSection />
